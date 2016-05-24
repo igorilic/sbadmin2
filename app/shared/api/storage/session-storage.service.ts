7d7574cddd87fd519e056f9e6738e0bd79d7ab9a
Igor Ilic <ilicigor@outlook.com>
2016-05-24 20:59:39 +0200

    greska u sessionstorageu

--- app/shared/api/storage/session-storage.service.ts
+++ app/shared/api/storage/session-storage.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
-import { BehaviorSubject } from 'rxjs/behaviorsubject';
 
 @Injectable()
 export class SessionStorageService {
